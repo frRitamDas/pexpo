6cce18fe533d6db7a4c815a58abd7b78b04088c2
frRitamDas <ritamdas20012@gmail.com>
2026-09-07 00:00:45 +0530

    Load dedicated player styles

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import "./pexpo-design.css";
+import "./pexpo-player.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "sonner";
