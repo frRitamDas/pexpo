e52bfb6369022b703f56983e400b7d71fd384b60
frRitamDas <ritamdas20012@gmail.com>
2026-09-06 23:37:48 +0530

    feat: match PEXPO reference logo

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="pexpo-logo" aria-label="PEXPO home">
-      <span className="pexpo-logo-mark" aria-hidden="true">P</span>
-      <span className="pexpo-logo-type">PEXPO</span>
+      <svg className="pexpo-logo-svg" viewBox="0 0 64 48" aria-hidden="true">
+        <path d="M8 10 C18 18 38 18 56 10" />
+        <path d="M6 22 C18 30 40 30 58 22" />
+        <path d="M4 34 C18 42 42 42 60 34" />
+      </svg>
     </Link>
   );
 }
